9dbcdf42ec263f31d249046097a30dcabf1984b7
amitsen7379 <amitkumar.in53@gmail.com>
2026-09-19 11:13:05 +0530

    Deploy: setup base url for mobile and desktop support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import react from '@vitejs/react-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
