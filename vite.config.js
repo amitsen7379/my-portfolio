20e18f0438e3ba140381b271476131e6bd547fd1
amitsen7379 <amitkumar.in53@gmail.com>
2026-09-19 11:52:40 +0530

    Deploy: setup base url for mobile and desktop support

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  base: '/my-portfolio/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-  base: '/my-portfolio/',
 })
